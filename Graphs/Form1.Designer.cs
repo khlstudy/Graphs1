0ae34ff126b928e9407c9f327053f02bf2486ef5
User <User@DESKTOP-4FAO2H6>
2023-06-10 22:55:26 +0300

    Add project files.

---
+++ Graphs/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Graphs
+{
+    partial class frmGraphics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series16 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbGraphics = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStep = new System.Windows.Forms.TextBox();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.chkFunc2 = new System.Windows.Forms.CheckBox();
+            this.chkFunc1 = new System.Windows.Forms.CheckBox();
+            this.chkCos2 = new System.Windows.Forms.CheckBox();
+            this.chkCos = new System.Windows.Forms.CheckBox();
+            this.chkSin = new System.Windows.Forms.CheckBox();
+            this.txtZnach = new System.Windows.Forms.TextBox();
+            this.btCount = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.btExit = new System.Windows.Forms.Button();
+            this.chartGraph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartGraph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbGraphics);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(880, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 135);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Значення графіка";
+            // 
+            // cbGraphics
+            // 
+            this.cbGraphics.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbGraphics.FormattingEnabled = true;
+            this.cbGraphics.Location = new System.Drawing.Point(6, 85);
+            this.cbGraphics.Name = "cbGraphics";
+            this.cbGraphics.Size = new System.Drawing.Size(241, 24);
+            this.cbGraphics.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(10, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 37);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Виберіть функцію значення \r\nякої треба порахувати:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtStep);
+            this.groupBox2.Controls.Add(this.txtMax);
+            this.groupBox2.Controls.Add(this.txtMin);
+            this.groupBox2.Location = new System.Drawing.Point(600, 31);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(274, 148);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Початкові дані";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введіть крок";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введіть Xmax";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введіть Xmin";
+            // 
+            // txtStep
+            // 
+            this.txtStep.Location = new System.Drawing.Point(139, 113);
+            this.txtStep.Name = "txtStep";
+            this.txtStep.Size = new System.Drawing.Size(100, 22);
+            this.txtStep.TabIndex = 2;
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(139, 66);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 22);
+            this.txtMax.TabIndex = 1;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(139, 21);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(100, 22);
+            this.txtMin.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chkFunc2);
+            this.groupBox3.Controls.Add(this.chkFunc1);
+            this.groupBox3.Controls.Add(this.chkCos2);
+            this.groupBox3.Controls.Add(this.chkCos);
+            this.groupBox3.Controls.Add(this.chkSin);
+            this.groupBox3.Location = new System.Drawing.Point(600, 185);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(274, 263);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Побудова графіків";
+            // 
+            // chkFunc2
+            // 
+            this.chkFunc2.AutoSize = true;
+            this.chkFunc2.Location = new System.Drawing.Point(22, 216);
+            this.chkFunc2.Name = "chkFunc2";
+            this.chkFunc2.Size = new System.Drawing.Size(156, 36);
+            this.chkFunc2.TabIndex = 4;
+            this.chkFunc2.Text = "Побудувати графік\r\n4+x+x^2";
+            this.chkFunc2.UseVisualStyleBackColor = true;
+            // 
+            // chkFunc1
+            // 
+            this.chkFunc1.AutoSize = true;
+            this.chkFunc1.Location = new System.Drawing.Point(22, 167);
+            this.chkFunc1.Name = "chkFunc1";
+            this.chkFunc1.Size = new System.Drawing.Size(156, 36);
+            this.chkFunc1.TabIndex = 3;
+            this.chkFunc1.Text = "Побудувати графік\r\nкв. корінь(x)";
+            this.chkFunc1.UseVisualStyleBackColor = true;
+            // 
+            // chkCos2
+            // 
+            this.chkCos2.AutoSize = true;
+            this.chkCos2.Location = new System.Drawing.Point(22, 121);
+            this.chkCos2.Name = "chkCos2";
+            this.chkCos2.Size = new System.Drawing.Size(156, 36);
+            this.chkCos2.TabIndex = 2;
+            this.chkCos2.Text = "Побудувати графік\r\ncos(x) + 2*x";
+            this.chkCos2.UseVisualStyleBackColor = true;
+            // 
+            // chkCos
+            // 
+            this.chkCos.AutoSize = true;
+            this.chkCos.Location = new System.Drawing.Point(22, 77);
+            this.chkCos.Name = "chkCos";
+            this.chkCos.Size = new System.Drawing.Size(156, 36);
+            this.chkCos.TabIndex = 1;
+            this.chkCos.Text = "Побудувати графік\r\ncos(x)\r\n";
+            this.chkCos.UseVisualStyleBackColor = true;
+            // 
+            // chkSin
+            // 
+            this.chkSin.AutoSize = true;
+            this.chkSin.Location = new System.Drawing.Point(22, 34);
+            this.chkSin.Name = "chkSin";
+            this.chkSin.Size = new System.Drawing.Size(156, 36);
+            this.chkSin.TabIndex = 0;
+            this.chkSin.Text = "Побудувати графік\r\nsin(x)\r\n";
+            this.chkSin.UseVisualStyleBackColor = true;
+            // 
+            // txtZnach
+            // 
+            this.txtZnach.Location = new System.Drawing.Point(887, 176);
+            this.txtZnach.Multiline = true;
+            this.txtZnach.Name = "txtZnach";
+            this.txtZnach.Size = new System.Drawing.Size(240, 392);
+            this.txtZnach.TabIndex = 6;
+            // 
+            // btCount
+            // 
+            this.btCount.Location = new System.Drawing.Point(633, 454);
+            this.btCount.Name = "btCount";
+            this.btCount.Size = new System.Drawing.Size(222, 34);
+            this.btCount.TabIndex = 7;
+            this.btCount.Text = "Розрахунок";
+            this.btCount.UseVisualStyleBackColor = true;
+            this.btCount.Click += new System.EventHandler(this.btCount_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(633, 494);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(222, 34);
+            this.btClear.TabIndex = 8;
+            this.btClear.Text = "Очистити графіки";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btClear_Click);
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(633, 534);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(222, 34);
+            this.btExit.TabIndex = 9;
+            this.btExit.Text = "Вихід";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.btExit_Click);
+            // 
+            // chartGraph
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chartGraph.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chartGraph.Legends.Add(legend4);
+            this.chartGraph.Location = new System.Drawing.Point(33, 31);
+            this.chartGraph.Name = "chartGraph";
+            series16.BorderWidth = 2;
+            series16.ChartArea = "ChartArea1";
+            series16.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series16.Legend = "Legend1";
+            series16.Name = "Sin (x)";
+            series17.BorderWidth = 2;
+            series17.ChartArea = "ChartArea1";
+            series17.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series17.Legend = "Legend1";
+            series17.Name = "Cos (x)";
+            series18.BorderWidth = 2;
+            series18.ChartArea = "ChartArea1";
+            series18.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series18.Legend = "Legend1";
+            series18.Name = "Cos (x) + 2*x";
+            series19.BorderWidth = 2;
+            series19.ChartArea = "ChartArea1";
+            series19.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series19.Legend = "Legend1";
+            series19.Name = "кв. корінь (x)";
+            series20.BorderWidth = 2;
+            series20.ChartArea = "ChartArea1";
+            series20.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series20.Color = System.Drawing.Color.Green;
+            series20.Legend = "Legend1";
+            series20.Name = "4x+x^2";
+            this.chartGraph.Series.Add(series16);
+            this.chartGraph.Series.Add(series17);
+            this.chartGraph.Series.Add(series18);
+            this.chartGraph.Series.Add(series19);
+            this.chartGraph.Series.Add(series20);
+            this.chartGraph.Size = new System.Drawing.Size(547, 518);
+            this.chartGraph.TabIndex = 10;
+            this.chartGraph.Text = "chart1";
+            // 
+            // frmGraphics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1164, 589);
+            this.Controls.Add(this.chartGraph);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btCount);
+            this.Controls.Add(this.txtZnach);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmGraphics";
+            this.Text = "Графіки";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartGraph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbGraphics;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStep;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox chkFunc2;
+        private System.Windows.Forms.CheckBox chkFunc1;
+        private System.Windows.Forms.CheckBox chkCos2;
+        private System.Windows.Forms.CheckBox chkCos;
+        private System.Windows.Forms.CheckBox chkSin;
+        private System.Windows.Forms.TextBox txtZnach;
+        private System.Windows.Forms.Button btCount;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartGraph;
+    }
+}
+
---
+++ Graphs/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphs
+{
+    public partial class frmGraphics : Form
+    {
+        public frmGraphics()
+        {
+            InitializeComponent();
+            cbGraphics.Items.Add("Графік Sin(x)");
+            cbGraphics.Items.Add("Графік Cos(x)");
+            cbGraphics.Items.Add("Графік Cos(x) + 2*x");
+            cbGraphics.Items.Add("Графік кв. корінь(x)");
+            cbGraphics.Items.Add("Графік 4+x+x^2");
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            chartGraph.Series[0].Points.Clear();
+            chartGraph.Series[1].Points.Clear();
+            chartGraph.Series[2].Points.Clear();
+            chartGraph.Series[3].Points.Clear();
+            chartGraph.Series[4].Points.Clear();
+        }
+
+        private void btCount_Click(object sender, EventArgs e)
+        {
+            txtZnach.Text = "";
+            double Xmin = 0;
+            double Xmax = 0;
+            double Step = 0;
+
+            if (txtMax.Text == "" || txtMax.Text == "" || txtStep.Text == "")
+            {
+                MessageBox.Show("Не всі дані введені!", "Помилка вводу");
+            }
+            else
+            {
+                Xmin = double.Parse(txtMin.Text);
+                Xmax = double.Parse(txtMax.Text);
+                Step = double.Parse(txtStep.Text);
+            }
+            
+            if (Xmin >= Xmax || Step <= 0)
+            {
+                MessageBox.Show("Помилка вводу даних!", "Помилка вводу");
+            }
+            else
+            {
+                int count = (int)Math.Ceiling((Xmax - Xmin) / Step) + 1;
+                double[] x = new double[count];
+                double[] y1 = new double[count];
+                double[] y2 = new double[count];
+                double[] y3 = new double[count];
+                double[] y4 = new double[count];
+                double[] y5 = new double[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    x[i] = Xmin + Step * i;
+                    y1[i] = Math.Sin(x[i]);
+                    y2[i] = Math.Cos(x[i]);
+                    y3[i] = Math.Cos(x[i]) + 2 * x[i];
+                    y4[i] = Math.Sqrt(x[i]);
+                    y5[i] = 4 + x[i] + (Math.Pow(x[i], 2));
+
+                    switch (cbGraphics.SelectedIndex)
+                    {
+                        case 0:
+                            txtZnach.Text += "x[" + i.ToString() + "] = " + Convert.ToString(x[i]) + "  " + "y1[" + i.ToString() + "] = " + Convert.ToString(Math.Round(y1[i], 5)) + Environment.NewLine;
+                            break;
+                        case 1:
+                            txtZnach.Text += "x[" + i.ToString() + "] = " + Convert.ToString(x[i]) + "  " + "y2[" + i.ToString() + "] = " + Convert.ToString(Math.Round(y2[i], 5)) + Environment.NewLine;
+                            break;
+                        case 2:
+                            txtZnach.Text += "x[" + i.ToString() + "] = " + Convert.ToString(x[i]) + "  " + "y3[" + i.ToString() + "] = " + Convert.ToString(Math.Round(y3[i], 5)) + Environment.NewLine;
+                            break;
+                        case 3:
+                            txtZnach.Text += "x[" + i.ToString() + "] = " + Convert.ToString(x[i]) + "  " + "y4[" + i.ToString() + "] = " + Convert.ToString(Math.Round(y4[i], 5)) + Environment.NewLine;
+                            break;
+                        case 4:
+                            txtZnach.Text += "x[" + i.ToString() + "] = " + Convert.ToString(x[i]) + "  " + "y5[" + i.ToString() + "] = " + Convert.ToString(Math.Round(y4[i], 5)) + Environment.NewLine;
+                            break;
+                        default: txtZnach.Text = "Графік не обрано!"; break;
+                    }
+                }
+                chartGraph.ChartAreas[0].AxisX.Minimum = Xmin;
+                chartGraph.ChartAreas[0].AxisX.Maximum = Xmax;
+
+                chartGraph.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+
+                if (chkSin.Checked) { chartGraph.Series[0].Points.DataBindXY(x, y1); }
+                if (chkCos.Checked) { chartGraph.Series[1].Points.DataBindXY(x, y2); }
+                if (chkCos2.Checked) { chartGraph.Series[2].Points.DataBindXY(x, y3); }
+                if (chkFunc1.Checked) { chartGraph.Series[3].Points.DataBindXY(x, y4); }
+                if (chkFunc2.Checked) { chartGraph.Series[4].Points.DataBindXY(x, y5); }
+                
+            }
+
+        }
+    }
+}
